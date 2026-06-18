9d781c9ef0ccb51ff72a5c2a594f80069dbfa9b2
--global <--global>
2026-06-18 19:53:55 -0300

    feat: adicionar regra da classificação

--- Filme.cs
+++ Filme.cs
@@ -5,12 +5,23 @@
     public int anoLancamento;
     public int classificacaoIndicativa;
 
+    public string ObterClassificacao()
+    {
+        if(classificacaoIndicativa <= 0)
+        {
+            return "livre";
+
+        }
+        return classificacaoIndicativa + "anos";
+    }
     public void ExibirInformacoes()
+
     {
         Console.WriteLine("\n=== Filme Cadastrado ===");
         Console.WriteLine($"Nome: {titulo}");
         Console.WriteLine($"Genero: {genero}");
         Console.WriteLine($"Ano que foi lançado:{anoLancamento}");
         Console.WriteLine($"Classificação: {classificacaoIndicativa}");
+        Console.WriteLine("Classificação: " + ObterClassificacao());
     }
 }
