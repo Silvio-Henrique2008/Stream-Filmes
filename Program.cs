0e91adf9f70d5a85f6363d356ae4ba4329471b03
--global <--global>
2026-06-18 21:33:38 -0300

    feat: implementa menu principal

--- Program.cs
+++ Program.cs
@@ -2,13 +2,52 @@ using System.Threading.Channels;
 
 Filme filme = new Filme();
 
-Console.Write("Titulo: ");
-filme.titulo = Console.ReadLine();
-Console.Write("Genero: " );
-filme.genero = Console.ReadLine();
-Console.Write("Ano de lançamento: ");
-filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
-Console.Write("Classificação Indicativa: ");
-filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
+int opcao;
+do
+{
+    Console.WriteLine("\n ======CATÁLOGO DE FILME======");
+    Console.WriteLine("1 - Cadastrar filme");
+    Console.WriteLine("2 - Exibir filme");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("Escolha uma opçõo");
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.Write("Titulo: ");
+            filme.titulo = Console.ReadLine();
+            Console.Write("Genero: ");
+            filme.genero = Console.ReadLine();
+            Console.Write("Ano de lançamento: ");
+            filme.anoLancamento = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Classificação Indicativa: ");
+            filme.classificacaoIndicativa = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nFilme cadastrado com sucesso!");
+            break;
+
+        case 2:
+            if (string.IsNullOrEmpty(filme.titulo))
+            {
+                Console.WriteLine("\nNenhum filme cadastrado.");
+            }
+            else
+            {
+                filme.ExibirInformacoes();
+            }
+            break;
+
+        case 0:
+            Console.WriteLine("\nPrograma encerrado.");
+            break;
+        default:
+            Console.WriteLine("\nOpção inválida");
+            break;
+    }
+} while (opcao != 0);
+
+
+
 
 filme.ExibirInformacoes();
